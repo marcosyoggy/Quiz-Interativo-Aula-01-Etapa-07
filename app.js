8d8fd67ae38b5a0a1ec3fe0ee832166f2de43fbd
marcosyoggy <44072541+marcosyoggy@users.noreply.github.com>
2021-08-18 10:52:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-const popup_close = document.querySelector('.popup_close')
+const popup_Close = document.querySelector('.popup_close')
 const popup_Ref = document.querySelector('.popup_Message')
 const popup_Content = document.querySelector('.popup_content')
 const count_Items_Ref = document.querySelector('.count_Items')
 const final_Score_Ref = document.querySelector('.final_Score')
-console.log(popup_Ref.classList)
+const form = document.querySelector('.user_action')
 
-const array_answers = ['B','A','C','A','B']
+const correct_Answers = ['B','A','C','A','B']
 let count_Score = 0
 let count_Items = 0
 
-const form = document.querySelector('.user_action')
+const button_Close = () => {
+    popup_Ref.setAttribute('style','display: none')
+    count_Items = 0
+    count_Score = 0
+}
 
-const insert_Content = data_In => {}
- 
-form.addEventListener('submit',event => {
+const sum_Score_Answers = (answer,index) => {
+    if(answer === correct_Answers[index]){
+        count_Score += 20
+        count_Items += 1
+    }
+}
+
+const submit_Form_User = event => {
     event.preventDefault()
+
     const input_Names = [
         form.q_number_1.value,
         form.q_number_2.value,
@@ -22,29 +32,50 @@ form.addEventListener('submit',event => {
         form.q_number_4.value,
         form.q_number_5.value
     ]
-//console.log(input_Names)//Exibe os valores das opções escolhidas;
-    input_Names.forEach((element,index) => {
-        if(element === array_answers[index]){
-            count_Score += 20
-            count_Items += 1
-        }
-    })
-        
+
+    input_Names.forEach(sum_Score_Answers)
  
-popup_Ref.style.display = 'block'
-// popup_Ref.setAttribute('class','display: block')
+    // popup_Ref.style.display = 'block'
+    popup_Ref.setAttribute('style','display: block')
+    count_Items_Ref.textContent = `Voce acertou: ${count_Items} questões.`
+    final_Score_Ref.textContent = `O valor total é: ${count_Score} pontos.`
+}
 
-count_Items_Ref.textContent = `Voce acertou: ${count_Items} questões.`
-final_Score_Ref.textContent = `O valor total é: ${count_Score} pontos.`
-})
 
-popup_close.addEventListener('click', event => {
-    event.stopPropagation()
-    // popup_Ref.setAttribute('style','display: none')
-    popup_Ref.style.display = 'none'
+/* ------------------------ SUBMIT -------------------------------- */
+form.addEventListener('submit', submit_Form_User)
+
+popup_Close.addEventListener('click', button_Close)
+        
+/* ---------------------------------------------------------------- */
+ 
+
+
+    /*
+    Este exercício será um pouquinho diferente dos anteriores.
+    
+    Seu desafio é desenvolver uma versão do quiz que:
+    
+    - Aborda um tema diferente (não pode ser de filmes);
+    - Tem um tema de cores diferente do que foi apresentado na aula;
+    - Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
+    
+    Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, tente fazer esse exercício sem rever partes da aula.
+    
+    É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
+    
+    Depois de fazer o que foi pedido acima, crie um repositório no GitHub para a sua aplicação e abra uma issue no repositório do curso com:
+    
+    - O link da sua versão do quiz;
+    - Quais foram as suas maiores dúvidas ou dificuldades durante a execução desse exercício;
+    - Quais foram as suas menores dificuldades durante a execução desse exercício.
+    
+    Link do repositório do curso: https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo/issues
+    
+    Ps: se você não conseguiu fazer tudo o que foi pedido acima, abra a issue mesmo assim =)
+    */
 
     
-})
 
  
 
@@ -177,26 +208,3 @@ popup_close.addEventListener('click', event => {
 
 
 
-/*
-Este exercício será um pouquinho diferente dos anteriores.
-
-Seu desafio é desenvolver uma versão do quiz que:
-
-- Aborda um tema diferente (não pode ser de filmes);
-- Tem um tema de cores diferente do que foi apresentado na aula;
-- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
-
-Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, tente fazer esse exercício sem rever partes da aula.
-
-É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
-
-Depois de fazer o que foi pedido acima, crie um repositório no GitHub para a sua aplicação e abra uma issue no repositório do curso com:
-
-- O link da sua versão do quiz;
-- Quais foram as suas maiores dúvidas ou dificuldades durante a execução desse exercício;
-- Quais foram as suas menores dificuldades durante a execução desse exercício.
-
-Link do repositório do curso: https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo/issues
-
-Ps: se você não conseguiu fazer tudo o que foi pedido acima, abra a issue mesmo assim =)
-*/
